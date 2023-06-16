50a33faa241025fe8b63bceb4c3fb9c0b381ec17
lucianoalessi9 <lucianoalessi9@hotmail.com>
2023-06-15 21:19:56 -0300

    feat: Add new React components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="drum-machine">
+
+      <div className='contenedor-principal'>hola
+
+        <div className='conetenedor-teclas'>hola</div>
+        <div className='contenedor-config'>chau</div>
+
+
+      </div>
       
     </div>
   );
