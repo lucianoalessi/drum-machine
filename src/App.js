5ce1a72de024b03c0d8ed720c6d81753d9790938
lucianoalessi9 <lucianoalessi9@hotmail.com>
2023-06-17 00:58:54 -0300

    feat: add the functions hanfleKeyPress and PlaySound

--- src/App.js
+++ src/App.js
@@ -62,10 +62,11 @@ function App() {
   }
 ];
 
-//Create a Conponet Button
+//Create a Component: Button
 
 function Button({ clip }) {
   const [active, setActive] = useState(false);
+
 
   useEffect(() => {
     document.addEventListener('keydown', handleKeyPress);
@@ -74,17 +75,36 @@ function Button({ clip }) {
     };
   }, []);
 
+
   const handleKeyPress = (e) => {
-    if(e.keycode === clip.keyCode)
-  }
+    if(e.keycode === clip.keyCode){
+      playSound();
+    }
+  };
 
 
+const playSound = () => {
+  const audioTag = document.getElementById(clip.keyTrigger);
+  setActive(true);
+  setTimeout(() => setActive(false), 200);
+  audioTag.currentTime = 0;
+  audioTag.play();
+};
 
 
-
-
+return (
+  <div
+  onClick={playSound}
+  className={`btn btn-secondary p-4 m-3 ${active && 'btn-warning'}`}
+  >
+    <audio className='clip' id={clip.keyTrigger} src={clip.url} />
+    {clip.keyTrigger}
+  </div>
+  );
 }
 
+
+//rendering
 
   return (
     <div className="drum-machine">
