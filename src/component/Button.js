74d724eff145701b0c54d949c1497aa561142f16
lucianoalessi9 <lucianoalessi9@hotmail.com>
2023-06-17 00:30:15 -0300

    first commit

--- src/component/Button.js
+++ src/component/Button.js
@@ -1,1 +1,14 @@
 import React from "react";
+import '../style-sheets/Button.css'
+
+
+function Button(props){
+    return(
+        <div className="button-container">
+            {props.children}
+        </div>
+    );
+}
+
+
+export default Button;
